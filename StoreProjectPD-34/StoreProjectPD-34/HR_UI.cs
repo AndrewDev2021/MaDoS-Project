8b4d2c2b9c32d03135ae16c164a20a67a6bbd1d3
Your Name <you@example.com>
2021-12-08 14:27:49 +0200

    java better

---
+++ StoreProjectPD-34/StoreProjectPD-34/HR_UI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StoreProjectPD_34
+{
+    class HR_UI 
+    {
+        public HRService _hRService;
+
+        public HR_UI(HRService hRService)
+        {
+            this._hRService = hRService;
+        }
+
+        public void CreateEmployee()
+        {
+            Console.Write("in CreateEmployee name?: ");
+            string name = Console.ReadLine();
+            Console.Write("CreateEmployee surname?: ");
+            string surName = Console.ReadLine();
+            Console.Write("CreateEmployee age?: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            _hRService.CreateHR(new HR(name, surName, age));
+        }
+    }
+}
